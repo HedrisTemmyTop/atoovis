2cb6da2eafd5aab2e06428408007048b5e0aea15
Shittu-Moshood <shittumoshoodademola@gmail.com>
2022-12-08 18:11:56 +0100

    congrat

--- src/components/vendor/Auth/Congrat.jsx
+++ src/components/vendor/Auth/Congrat.jsx
@@ -1,10 +1,41 @@
 import React from "react";
-
+import putin from '../../../assets/images/Header//Group (4).png'
 const Congrat =()=>{
     return(
-        <div>
-            Congrat
+        <div style={{display: 'flex'}}>
+        <div style={{width: 555, height: 'auto', borderRadius:27, background: '#fff', margin: 40, padding: 40, alignItems: 'center', display: 'flex', flexDirection: 'column'}}>
+        
+            <img src={putin} alt="" width={64} height={64} />
+        <h1 style={{marginBottom: 10, textAlign: 'center', color: '#000', fontSize: 24, fontWeight: 800, marginTop: 20}}>CONGRATULATIONS...</h1>
+        <p style={{marginBottom: 10, textAlign: 'center', color: '#000', fontSize: 14, fontWeight: 500, marginBottom: 20}}>
+        Your membership portal has been created!</p>
+        <p style={{marginBottom: 10, textAlign: 'center', color: '#000', fontSize: 14, fontWeight: 500}}>
+        What next?</p>
+        <p style={{marginBottom: 10, textAlign: 'center', color: '#000', fontSize: 16, fontWeight: 500}}>
+        Upload and sell your product</p>
+
+       
+         
+
+        <p style={{marginBottom: 10, textAlign: 'center', color: '#495454', fontSize: 14, fontWeight: 400}}>
+        Now that your content portal has been created, it’s time to charge for it! The system will automatically email your new customers their username and password to the portal. 
+        Proceed to create/edit your product at the My Products page.</p> 
+
+               
+            
+           <button onClick={()=> setValue(10)} style={{background: '#4CC5D2',
+          color: '#fff',
+          width: 495,
+          height: 66,
+          borderRadius: 100,
+          marginTop: 20
+          }}>Go to Products</button>
+          
+            
+         
         </div>
+
+     </div>
     )
 }
 
--- src/components/vendor/Auth/Identity.jsx
+++ src/components/vendor/Auth/Identity.jsx
@@ -1,10 +1,73 @@
 import React from "react";
 
-const Identity =()=>{
+const Identity =({setValue})=>{
     return(
-        <div>
-            identity
+        <div style={{display: 'flex'}}>
+        <div style={{width: 555, height: 'auto', borderRadius:27, background: '#fff', margin: 40, padding: 40, alignItems: 'left', display: 'flex', flexDirection: 'column'}}>
+        <h1 style={{marginBottom: 10, textAlign: 'left', color: '#000', fontSize: 32}}>Identity Verification</h1>
+        <p style={{marginBottom: 10, textAlign: 'left', color: '#000', fontSize: 12}}>
+            Please choose one of the available options to connect
+             with an Atoovis associate to complete your verification.</p>
+         
+
+            <form action="">
+                <div style={{display: 'flex'}}>
+                  <div>
+                    <input type="radio" name="schedule"/>
+                  </div>
+                  <div style={{marginLeft: 10}}>
+                    <div style={{display: 'flex', justifyContent:'space-between', width:'100%'}}>
+                      <div><p style={{fontSize: 14, fontWeight: 700}}>Scheduled Video Call</p></div>
+                      <div><p style={{fontSize: 12, fontWeight: 500}}>Wait time: 2-4 days</p></div>
+                    </div>
+                    <p style={{fontSize: 12, fontWeight: 500}}>Schedule an appointment with an Atoovis
+                       Associate on a future date. Carry your 
+                       original government issued photo ID document 
+                       and other documents you uploaded during registration</p>
+                  </div>
+                </div>
+
+                <div style={{display: 'flex', marginTop: 10}}>
+                  <div>
+                    <input type="radio" name="schedule"/>
+                  </div>
+                  <div style={{marginLeft: 10}}>
+                    <div style={{display: 'flex', justifyContent:'space-between', width:'100%'}}>
+                      <div><p style={{fontSize: 14, fontWeight: 700}}>Instant Video Call</p></div>
+                      <div><p style={{fontSize: 12, fontWeight: 500}}>Wait time: 9am -5pm</p></div>
+                    </div>
+                    <p style={{fontSize: 12, fontWeight: 500}}>Schedule an appointment with an Atoovis
+                       Associate on a future date. Carry your 
+                       original government issued photo ID document 
+                       and other documents you uploaded during registration</p>
+                  </div>
+                </div>
+            <button onClick={()=> setValue(8)} style={{
+          color: '#fff',
+          width: 495,
+          height: 66,
+          borderRadius: 100,
+          borderColor: '#4CC5D2',
+          marginTop: 20,
+          color: '#4CC5D2',
+          }}>Previous</button>
+           <button onClick={()=> setValue(10)} style={{background: '#4CC5D2',
+          color: '#fff',
+          width: 495,
+          height: 66,
+          borderRadius: 100,
+          marginTop: 20
+          }}>Continue</button>
+            </form>
+      
+            
+         
         </div>
+
+        <div>
+          faqs
+        </div>
+     </div>
     )
 }
 
