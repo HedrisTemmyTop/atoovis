09cfb6bb264357b5a4c9911fec88cb1895934e39
OnlynfK <onlygod15@hotmail.com>
2022-12-11 05:45:16 +0100

    setupping up redux

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import { BrowserRouter} from "react-router-dom";
 import App from './App'
 // import Vendor from './pages/VendorLanding'
 import './index.css'
-
+import {store} from './app/store'
+import { Provider } from 'react-redux';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
     </BrowserRouter>
    
   </React.StrictMode>
