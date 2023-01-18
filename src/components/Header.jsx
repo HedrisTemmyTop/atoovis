9a81d653c452b7cf4104abea8ae409217c5b6e5b
[ <hedristemitope2001@gmail.com>
2023-01-18 15:41:12 +0100

    Home page responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -114,7 +114,7 @@ const Header = () => {
         // className=" mode"
       >
         {/* <SignIn /> */}
-        <SignUp />
+        {/* <SignUp /> */}
         {/* <NumberOtp/> */}
         {/* <EmailOtp/> */}
         {/* <Redirect/> */}
